d5bed5148ec1f00dcc80ae4685090d6c8ba1c403
Alex Wauters <alex.wauters@gmail.com>
2016-12-04 14:21:30 +0100

    go fmt

--- day 04/Alex - Go/obscurity-pt2.go
+++ day 04/Alex - Go/obscurity-pt2.go
@@ -1,26 +1,25 @@
 package main
 
 import (
-	"strings"
 	"fmt"
+	"strings"
 )
 
 func part2(rooms []Room) {
 	for _, room := range rooms {
-		if(room.Decrypt() == "northpole object storage") {
+		if room.Decrypt() == "northpole object storage" {
 			fmt.Printf("The north pole has sector id: %v", room.sector)
 			return
 		}
 	}
 }
 
-
 func (r Room) Decrypt() string {
 	return strings.Map(func(ru rune) rune {
 		if ru == ' ' {
 			return ru
 		}
-		shifted := int(ru)+r.sector%26
+		shifted := int(ru) + r.sector%26
 		if shifted > 'z' {
 			return rune(shifted - 26)
 		}
