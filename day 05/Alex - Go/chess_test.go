bc92a7d19be64fd0402105f472f8d2ca60e4e166
Alex Wauters <alex.wauters@gmail.com>
2016-12-05 17:45:06 +0100

    Removed quit check on findGoodHash

--- day 05/Alex - Go/chess_test.go
+++ day 05/Alex - Go/chess_test.go
@@ -4,11 +4,13 @@ import (
 	"testing"
 )
 
-func TestHash(t *testing.T){
+func TestHash(t *testing.T) {
 	result := hash("abc", 3231929)
-	if result != "00000155f8105dff7f56ee10fa9b9abd" { t.Error("Wrong hash")}
+	if result != "00000155f8105dff7f56ee10fa9b9abd" {
+		t.Error("Wrong hash")
+	}
 	c := make(chan string)
-	go findGoodHash("abc", 3231929, c, make(chan bool))
+	go findGoodHash("abc", 3231929, c)
 	letter := <-c
 	if letter != "1" {
 		t.Error("Did not receive correct letter from hash")
