0609be9791ecc825396eb5e95bf48cecba2f3835
Alex Wauters <alex.wauters@gmail.com>
2016-12-10 17:50:04 +0100

    Day 10: Simplified a bit

--- day 10/Alex - Go/bots.go
+++ day 10/Alex - Go/bots.go
@@ -10,7 +10,6 @@ import (
 )
 
 var handover, inputbin *regexp.Regexp
-var answerFound func(int, int) bool
 var answer chan int
 var bins map[int]int
 
@@ -21,24 +20,17 @@ func main() {
 
 func part1() {
 	defer track(time.Now(), "part 1")
-	answerFound = endCondition(61, 17)
 	answer = make(chan int)
 	commands := make(chan command, 50)
 	go readInput(commands)
 	go dispatcher(commands)
-	droid := <-answer
-	fmt.Printf("The droid you're looking for is %v \n", droid)
+	droidId := <-answer
+	fmt.Printf("The droid you're looking for is %v \n", droidId)
 }
 
 type command struct {
-	value
-	valueCmd string
-	source   int
-}
-
-type value struct {
-	destType          string
-	destNumber, value int
+	valueCmd, destType        string
+	source, destNumber, value int
 }
 
 type droid struct {
@@ -51,24 +43,21 @@ type droid struct {
 //A balance bot can receive up to two values and then pass them on
 func bot(droid *droid) {
 	val1, val2 := <-droid.input, <-droid.input
-	if answerFound(val1, val2) {
-		answer <- droid.id
-	}
 	if val2 < val1 {
 		val1, val2 = val2, val1
 	}
+	if answerFound(val1, val2) {
+		answer <- droid.id
+	}
 	cmd1, cmd2 := <-droid.commands, <-droid.commands
 	if cmd1.valueCmd == "low" {
-		cmd1.value.value, cmd2.value.value = val1, val2
-		cmd1.valueCmd, cmd2.valueCmd = "", ""
-		droid.output <- cmd1
-		droid.output <- cmd2
+		cmd1.value, cmd2.value = val1, val2
 	} else {
-		cmd1.value.value, cmd2.value.value = val2, val1
-		cmd1.valueCmd, cmd2.valueCmd = "", ""
-		droid.output <- cmd1
-		droid.output <- cmd2
+		cmd1.value, cmd2.value = val2, val1
 	}
+	cmd1.valueCmd, cmd2.valueCmd = "", ""
+	droid.output <- cmd1
+	droid.output <- cmd2
 }
 
 //Dispatcher sends commands from the input to bots or values (received from drones) to drones/output bins
@@ -82,9 +71,9 @@ func dispatcher(commands chan command) {
 		} else { // Send a value to a bot
 			if cmd.destType == "bot" {
 				bot := getBot(droids, cmd.destNumber, commands)
-				bot.input <- cmd.value.value
+				bot.input <- cmd.value
 			} else {
-				bins[cmd.destNumber] = cmd.value.value
+				bins[cmd.destNumber] = cmd.value
 			}
 		}
 	}
@@ -102,10 +91,8 @@ func getBot(droids map[int]droid, id int, output chan command) *droid {
 	return &drone
 }
 
-func endCondition(i, j int) func(int, int) bool {
-	return func(k, m int) bool {
-		return k == i && m == j || k == j && m == i
-	}
+func answerFound(k, m int) bool {
+	return k == 17 && m == 61
 }
 
 //Read the input, create one or more commands from each input line
@@ -124,14 +111,13 @@ func readInput(commands chan command) {
 func sendCommands(line string, commands chan command) {
 	cmd := handover.FindStringSubmatch(line) //1=botid 2=loworhigh 3=botoroutput 4=destid 5=loworhighh 6=botoroutput 7=destid2
 	if len(cmd) > 0 {
-		commands <- command{source: toInt(cmd[1]), valueCmd: cmd[2], value: value{destType: cmd[3], destNumber: toInt(cmd[4])}}
-		commands <- command{source: toInt(cmd[1]), valueCmd: cmd[5], value: value{destType: cmd[6], destNumber: toInt(cmd[7])}}
+		commands <- command{source: toInt(cmd[1]), valueCmd: cmd[2], destType: cmd[3], destNumber: toInt(cmd[4])}
+		commands <- command{source: toInt(cmd[1]), valueCmd: cmd[5], destType: cmd[6], destNumber: toInt(cmd[7])}
 	} else if cmd = inputbin.FindStringSubmatch(line); len(cmd) > 0 { //1=value 2=botid
-		commands <- command{value{value: toInt(cmd[1]), destNumber: toInt(cmd[2]), destType: "bot"}, "", 0}
+		commands <- command{value: toInt(cmd[1]), destNumber: toInt(cmd[2]), destType: "bot"}
 	} else {
 		panic("Could not parse: " + line)
 	}
-
 }
 
 func toInt(s string) (i int) {
