fb4cb070e91aac27de83c8d7dde57097525276a0
jhgbrt <jeroen@haegebaert.com>
2016-12-10 21:54:27 +0100

    day 2 part 1

---
+++ day 02/Jeroen - C#/EasterBunny.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Jeroen.Day2
+{
+    public class Keypad
+    {
+        public Keypad(int initial)
+        {
+            Current = initial;
+        }
+
+        public int Current { get; private set; }
+
+        public void Move(char direction)
+        {
+            // 1 2 3
+            // 4 5 6
+            // 7 8 9
+
+            switch (direction)
+            {
+                case 'U' when new[] { 1, 2, 3}.Contains(Current):
+                case 'L' when new[] { 1, 4, 7 }.Contains(Current):
+                case 'D' when new[] { 7, 8, 9 }.Contains(Current):
+                case 'R' when new[] { 3, 6, 9 }.Contains(Current):
+                    Current = Current;
+                    break;
+                case 'U':
+                    Current = Current - 3;
+                    break;
+                case 'D':
+                    Current = Current + 3;
+                    break;
+                case 'L':
+                    Current = Current - 1;
+                    break;
+                case 'R':
+                    Current = Current + 1;
+                    break;
+            }
+        }
+    }
+}
